4f1db96cc5d1a69f537fb48e8f69c3c4f3099ff1
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2025-05-14 23:57:51 +0500

    code

--- 02FlutterLectures/lecture02mushaf/lib/IndexSurahSCR.dart
+++ 02FlutterLectures/lecture02mushaf/lib/IndexSurahSCR.dart
@@ -14,29 +14,52 @@ class _IndexSurahSCRState extends State<IndexSurahSCR> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Image.asset(
+            "assets/images/quran.png",
+            width: 100,
+            height: 100,
+          ),
+        ),
         body: ListView.builder(
-      // itemCount: 114,
-      itemCount: quran.totalSurahCount,
-      itemBuilder: (context, index) {
-        return ListTile(
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailSurah(index + 1),
-                ));
+          // itemCount: 114,
+          itemCount: quran.totalSurahCount,
+          itemBuilder: (context, index) {
+            return ListTile(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailSurah(index + 1),
+                    ));
+              },
+              leading: CircleAvatar(
+                child: Text("${index + 1}"),
+              ),
+              title: Text(
+                quran.getSurahNameArabic(index + 1),
+                style: GoogleFonts.amiriQuran(),
+              ),
+              subtitle: Text(quran.getSurahName(index + 1)),
+              trailing: Column(
+                children: [
+                  quran.getPlaceOfRevelation(index + 1) == "Makkah"
+                      ? Image.asset(
+                          "assets/images/kaaba.png",
+                          width: 30,
+                          height: 30,
+                        )
+                      : Image.asset(
+                          "assets/images/madina.png",
+                          width: 30,
+                          height: 30,
+                        ),
+                  Text("Verses" + quran.getVerseCount(index + 1).toString()),
+                ],
+              ),
+            );
           },
-          leading: CircleAvatar(
-            child: Text("${index + 1}"),
-          ),
-          title: Text(
-            quran.getSurahNameArabic(index + 1),
-            style: GoogleFonts.amiriQuran(),
-          ),
-          subtitle: Text(quran.getSurahName(index + 1)),
-          trailing: Text(quran.getVerseCount(index + 1).toString()),
-        );
-      },
-    ));
+        ));
   }
 }

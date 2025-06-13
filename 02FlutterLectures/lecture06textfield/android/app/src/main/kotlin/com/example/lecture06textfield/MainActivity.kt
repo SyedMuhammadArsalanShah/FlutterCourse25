7f279290276bb4fcf6c25c114cda262ef9d73f86
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2025-06-13 20:48:46 +0500

    lec 06

---
+++ 02FlutterLectures/lecture06textfield/android/app/src/main/kotlin/com/example/lecture06textfield/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lecture06textfield
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ 02FlutterLectures/lecture06textfield/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Login(),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController email = TextEditingController();
+  TextEditingController pass = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          TextField(controller: email),
+          TextField(controller: pass),
+          SizedBox(height: 10,),
+          ElevatedButton(
+            onPressed: () {
+              String emailaddress = email.text.toString();
+              String password = pass.text.toString();
+
+              print("My Email is " + emailaddress);
+              print("My Password is " + password);
+            },
+            child: Text("Login"),
+          ),
+        ],
+      ),
+    );
+  }
+}
